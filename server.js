9d8f46c1d057f2db2c81e4c4c63ef8f287667cb9
Titilayo Olorunsaiye <toolorunsaiye@gmail.com>
2024-08-29 21:32:01 +0100

    remove __dirname

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ const getInfo = require("get-artist-title");
 const likedDetails = [];
 let uris = [];
 
-app.use(express.static(path.join(__dirname, "./public")));
+// app.use(express.static(path.join(__dirname, "./public")));
+
+app.use(express.static("./public"));
 
 app.get("/", function (req, res) {
   res.sendFile("/index.html");
